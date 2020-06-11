24bbdc68d2ef18672df886adc3c78fc4b2564c1d
Vavarooks <vavarooks123@gmail.com>
2020-06-11 16:11:14 -0700

    updates

--- client/src/componet/WelcomePage.js
+++ client/src/componet/WelcomePage.js
@@ -3,7 +3,9 @@ import React from "react";
 function WelcomePage(){
     return(
         <div className="welcome">
+            <div className="text">
             <h1>Welcome To Google Book Search! Start your search here.</h1>
+            </div>
         </div>   
          )
 }
